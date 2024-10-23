b6dbcb37db2a86d317f831b65ee2dfb70ce98c12
uchejames <uchejamesjac@gmail.com>
2024-10-23 17:39:59 +0100

    first commit

---
+++ kioskapp2/lib/video_player/barbellvideo/barbellvideo_widget.dart
@@ -0,0 +1,121 @@
+import '/backend/backend.dart';
+import '/components/header/header_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_youtube_player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'barbellvideo_model.dart';
+export 'barbellvideo_model.dart';
+
+class BarbellvideoWidget extends StatefulWidget {
+  const BarbellvideoWidget({
+    super.key,
+    this.barbellrecieved,
+  });
+
+  final DocumentReference? barbellrecieved;
+
+  @override
+  State<BarbellvideoWidget> createState() => _BarbellvideoWidgetState();
+}
+
+class _BarbellvideoWidgetState extends State<BarbellvideoWidget> {
+  late BarbellvideoModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => BarbellvideoModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<BarbellRecord>(
+      stream: FFAppState().barbellVideo(
+        requestFn: () => BarbellRecord.getDocument(widget.barbellrecieved!),
+      ),
+      builder: (context, snapshot) {
+        // Customize what your widget looks like when it's loading.
+        if (!snapshot.hasData) {
+          return Scaffold(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            body: Center(
+              child: SizedBox(
+                width: 50.0,
+                height: 50.0,
+                child: SpinKitDoubleBounce(
+                  color: FlutterFlowTheme.of(context).primary,
+                  size: 50.0,
+                ),
+              ),
+            ),
+          );
+        }
+
+        final barbellvideoBarbellRecord = snapshot.data!;
+
+        return YoutubeFullScreenWrapper(
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+              key: scaffoldKey,
+              backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+              body: SafeArea(
+                top: true,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 30.0),
+                        child: wrapWithModel(
+                          model: _model.headerModel,
+                          updateCallback: () => safeSetState(() {}),
+                          child: const HeaderWidget(),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 50.0, 0.0, 0.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 200.0,
+                          decoration: BoxDecoration(
+                            color: FlutterFlowTheme.of(context)
+                                .secondaryBackground,
+                          ),
+                          child: FlutterFlowYoutubePlayer(
+                            url: barbellvideoBarbellRecord.video,
+                            autoPlay: false,
+                            looping: true,
+                            mute: false,
+                            showControls: true,
+                            showFullScreen: true,
+                            strictRelatedVideos: true,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
